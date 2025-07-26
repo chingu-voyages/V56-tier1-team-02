08fb75f2d67d63fc3eec5fd3bb2e32fa405c99bf
ouassima <elyakoubi.ouassima@gmail.com>
2025-07-26 15:49:28 +0100

    initial project setup with routing

---
+++ src/pages/PatientStatus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PatientStatus() {
+  return (
+    <div>PatientStatus</div>
+  )
+}
+
+export default PatientStatus
---
+++ src/pages/StatusUpdate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StatusUpdate() {
+  return (
+    <div>StatusUpdate</div>
+  )
+}
+
+export default StatusUpdate
