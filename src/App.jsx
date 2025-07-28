3d56f55870cbc1a90e215f9e005ba53e91dc1d5f
Ouassima El yakoubi <elyakoubi.ouassima@gmail.com>
2025-07-28 16:10:51 +0100

    creating responsive home page (#15)

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,18 @@ import Home from './pages/Home'
 import PatientStatus from './pages/PatientStatus'
 import PatientInfo from './pages/PatientInfo'
 import StatusUpdate from './pages/StatusUpdate'
+import Login from './pages/Login'
 
 function App() {
 
   return (
     <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/status" element={<PatientStatus />} />
+          <Route path="/patient-status" element={<PatientStatus />} />
           <Route path="/info" element={<PatientInfo />} />
           <Route path="/update" element={<StatusUpdate />} />
+          <Route path="/login" element={<Login />} />
+          {/* Add more routes as needed */}
     </Routes>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
-function Home() {
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleGuest = () => {
+    localStorage.setItem("role", "guest"); // Set role
+    navigate("/patient-status"); // Redirect to Patient Status page
+  };
+
+  const handleLogin = () => {
+    navigate("/login"); // Goes to login page to set role as admin/surgical
+  };
+
   return (
-    <h1>Home</h1>
-  )
-}
-
-export default Home
+    <div className="flex flex-col items-center justify-center min-h-screen m-2 lg:m-0" >
+      <h1 className="text-2xl md:text-4xl text-center font-bold mb-8 text-[#0069AB] leading-relaxed lg:w-[60%]">Track the process of your loved one’s surgery in real-time</h1>
+        <div className="flex items-center justify-center self-center flex-col gap-4  md:w-95 xl:w-105 xl:py-12 px-6 rounded-lg bg-white shadow-xl py-10 w-[90%]">
+          <button onClick={handleLogin} className="border-2 bg-[#0069AB] border-[#045679] font-bold text-white px-10 py-2 rounded-lg text-md md:text-lg xl:text-xl">Log In</button>
+          <div className="flex items-center justify-center gap-4 w-full px-8 py-3 text-[#367B99]">
+            <hr className="w-1/2 border-1"/>
+            <p className="font-bold md:text-xl text-lg xl:text-2xl">Or</p>
+            <hr className="w-1/2 border-1"/>
+          </div>
+          <button onClick={handleGuest} className="border-2 text-[#367B99] border-[#367B99] font-bold bg-white rounded-lg px-8 py-2 text-md md:text-lg xl:text-xl">Continue as Guest</button>
+        </div>
+    </div>
+  );
+};
+export default Home;
