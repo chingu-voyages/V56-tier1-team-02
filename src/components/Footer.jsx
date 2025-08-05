4e5f1c2e338a2dcbab0e83d6fc2c216beeacab00
Ouassima El yakoubi <elyakoubi.ouassima@gmail.com>
2025-08-05 02:06:52 +0100

    create header and not found page (#18)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import { FaGithub } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <footer className='text-[#0069AB] bg-white/50 flex items-center justify-center py-2 gap-2'>
-      <p>Built By <a href="https://github.com/Isaacjosh23" target="_blank">Josh, </a>
+    <footer className='text-[#0069AB] bg-white/50 flex items-center justify-center py-3 gap-2 md:px-0 px-2 md:text-md text-[15px]'>
+      <p className=''>Built By <a href="https://github.com/Isaacjosh23" target="_blank">Josh, </a>
       <a href="https://github.com/ouassimaelyakoubi" target="_blank">Ouassima </a>and
       <a href="https://www.linkedin.com/in/agilem" target="_blank"> Emily</a>
       </p>
 
-      <div className='flex items-center gap-2'>
+      <div className='flex items-center md:gap-2 gap-1'>
         <p className='text-[#0069AB]'>|</p>
         <FaGithub />
         <a href="https://github.com/chingu-voyages/V56-tier1-team-02" target="_blank"> View on GitHub</a>
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const role = localStorage.getItem("role");
+
+  if (!role) return <Navigate to="/login" />;
+  if (!allowedRoles.includes(role)) return <Navigate to="/" />;
+
+  return children;
+};
+
+export default ProtectedRoute;
