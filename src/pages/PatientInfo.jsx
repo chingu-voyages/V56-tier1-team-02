43e7a6c1d27866591a83872fff3badec647c4904
Isaacjosh23 <isaacjoshua969@gmail.com>
2025-08-09 19:34:54 +0100

    showhideforms

--- src/pages/PatientInfo.jsx
+++ src/pages/PatientInfo.jsx
@@ -1,214 +1,372 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PatientInfo = () => {
-  return (
-    <div className="px-6 py-10">
-      <PatientInfoBtn />
+  const [hidePatientBtn, setHidePatientBtn] = useState(true);
+  const [showAddPatient, setShowAddPatient] = useState(false);
+  const [showSearchPatient, setShowSearchPatient] = useState(false);
 
-      <PatientInfoAddForm />
-      <PatientInfoSearchForm />
+  // Function Open Add Patient Form
+  const handleShowAddPatient = () => {
+    setShowAddPatient(!showAddPatient);
+
+    setHidePatientBtn(!hidePatientBtn);
+  };
+
+  const handleHideAddPatient = () => {
+    setShowAddPatient(!showAddPatient);
+
+    setHidePatientBtn(true);
+  };
+
+  // Function to Show Search Patient Form
+  const handleShowSearchPatient = () => {
+    setShowSearchPatient(!showSearchPatient);
+
+    setHidePatientBtn(!hidePatientBtn);
+  };
+
+  const handleHideSearchPatient = () => {
+    setShowSearchPatient(!showSearchPatient);
+
+    setHidePatientBtn(!hidePatientBtn);
+  };
+
+  return (
+    <div className="px-6 py-10 flex flex-col justify-center patient-info">
+      {hidePatientBtn && (
+        <PatientInfoBtn
+          onShowAddPatient={handleShowAddPatient}
+          onShowSearchPatient={handleShowSearchPatient}
+        />
+      )}
+
+      <div>
+        {showAddPatient && (
+          <PatientInfoAddForm onHideAddPatient={handleHideAddPatient} />
+        )}
+        {showSearchPatient && (
+          <PatientInfoSearchForm
+            onHideSearchPatient={handleHideSearchPatient}
+          />
+        )}
+      </div>
     </div>
   );
 };
 
-const PatientInfoBtn = () => {
+const PatientInfoBtn = ({ onShowAddPatient, onShowSearchPatient }) => {
   return (
-    <div className="flex flex-col items-center gap-4">
-      <Button>Add New Patient</Button>
+    <div className="flex items-center max-sm:flex-col justify-center gap-4">
+      <Button onClick={onShowAddPatient}>Add New Patient</Button>
 
-      <Button secondary>Search Existing Patient</Button>
+      <Button secondary onClick={onShowSearchPatient}>
+        Search Existing Patient
+      </Button>
     </div>
   );
 };
 
-const PatientInfoAddForm = () => {
+const PatientInfoAddForm = ({ onHideAddPatient }) => {
   return (
-    <form className="bg-white px-6 py-4 flex flex-col gap-6 add-patient-form">
-      <h4 className="text-xs text-[var(--color-main)]">
+    <form className="bg-white w-[500px] max-sm:w-[290px] px-6 py-4 m-auto flex flex-col items-center max-sm:items-stretch gap-6 add-patient-form">
+      <h4 className="text-[14px] text-[var(--color-main)] font-medium">
         No patients have been added it yet. Use the form below to get started
       </h4>
 
       <div className="flex flex-col gap-2.5">
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="first-name"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            First Name
-          </label>
-          <input id="first-name" type="text" className="px-3 text-[14px]" />
+        <div className="grid grid-cols-2 gap-x-6 max-sm:grid-cols-1">
+          <div className="flex flex-col gap-1 add-input">
+            <label
+              htmlFor="first-name"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              First Name
+            </label>
+            <input
+              id="first-name"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 add-input">
+            <label
+              htmlFor="last-name"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              Last Name
+            </label>
+            <input
+              id="last-name"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
         </div>
 
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="last-name"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            Last Name
-          </label>
-          <input id="last-name" type="text" className="px-3 text-[14px]" />
-        </div>
-
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 add-input">
           <label
             htmlFor="address"
             className="text-[var(--color-main)] text-[12px] font-medium"
           >
             Street Address
           </label>
-          <input id="address" type="text" className="px-3 text-[14px]" />
+          <input
+            id="address"
+            type="text"
+            className="px-3 text-[14px] font-semibold"
+          />
         </div>
 
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="state"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            State
-          </label>
-          <input id="state" type="text" className="px-3 text-[14px]" />
+        <div className="grid grid-cols-2 gap-x-6 gap-y-2.5 max-sm:grid-cols-1">
+          <div className="flex flex-col gap-1 add-input">
+            <label
+              htmlFor="state"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              State
+            </label>
+            <input
+              id="state"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 add-input">
+            <label
+              htmlFor="city"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              City
+            </label>
+            <input
+              id="city"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 add-input">
+            <label
+              htmlFor="country"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              Country
+            </label>
+            <input
+              id="country"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 add-input">
+            <label
+              htmlFor="number"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              Phone Number
+            </label>
+            <input
+              id="number"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
         </div>
 
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="country"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            Country
-          </label>
-          <input id="country" type="text" className="px-3 text-[14px]" />
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="number"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            Phone Number
-          </label>
-          <input id="number" type="text" className="px-3 text-[14px]" />
-        </div>
-
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 add-input">
           <label
             htmlFor="email"
             className="text-[var(--color-main)] text-[12px] font-medium"
           >
             Contact Email
           </label>
-          <input id="email" type="text" className="px-3 text-[14px]" />
+          <input
+            id="email"
+            type="text"
+            className="px-3 text-[14px] font-semibold"
+          />
         </div>
       </div>
 
-      <button className="py-[5px] px-[10px] text-[14px] bg-[var(--color-main)] text-white font-semibold rounded-[6px] cursor-pointer">
-        Add Patient
-      </button>
+      <div className="flex justify-center items-center gap-8">
+        <button className="py-[5px] px-[10px] text-[14px] font-semibold bg-[var(--color-main)] text-white  rounded-[6px] cursor-pointer">
+          Add Patient
+        </button>
+
+        <button
+          className="py-[5px] px-[10px] text-[14px] font-semibold bg-[#d9d9d9] text-white  rounded-[6px] cursor-pointer"
+          onClick={onHideAddPatient}
+        >
+          Close
+        </button>
+      </div>
     </form>
   );
 };
 
-const PatientInfoSearchForm = () => {
+const PatientInfoSearchForm = ({ onHideSearchPatient }) => {
   return (
-    <form className="bg-white px-6 py-4 flex flex-col gap-6 search-patient-form">
+    <form className="bg-white w-[500px] max-sm:w-[290px] px-6 py-4 m-auto flex flex-col items-center max-sm:items-stretch gap-6 add-patient-form">
       <div className="flex flex-col gap-2.5">
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="search"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
+        <div className="flex flex-col gap-1 search-input">
+          <label className="text-[var(--color-main)] font-medium text-[12px]">
             Search Patient By ID
           </label>
+
           <input
-            id="search"
+            id="first-name"
             type="text"
-            className="px-3 text-[14px] search-input font-semibold"
+            className="px-3 text-[14px] font-semibold search-patient"
             placeholder="CF8902"
           />
         </div>
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="first-name"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            First Name
-          </label>
-          <input id="first-name" type="text" className="px-3 text-[14px]" />
+
+        <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-x-6">
+          <div className="flex flex-col gap-1 search-input">
+            <label
+              htmlFor="first-name"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              First Name
+            </label>
+            <input
+              id="first-name"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 search-input">
+            <label
+              htmlFor="last-name"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              Last Name
+            </label>
+            <input
+              id="last-name"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
         </div>
 
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="last-name"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            Last Name
-          </label>
-          <input id="last-name" type="text" className="px-3 text-[14px]" />
-        </div>
-
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 search-input">
           <label
             htmlFor="address"
             className="text-[var(--color-main)] text-[12px] font-medium"
           >
             Street Address
           </label>
-          <input id="address" type="text" className="px-3 text-[14px]" />
+          <input
+            id="address"
+            type="text"
+            className="px-3 text-[14px] font-semibold"
+          />
         </div>
 
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="state"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            State
-          </label>
-          <input id="state" type="text" className="px-3 text-[14px]" />
+        <div className="grid grid-cols-2 gap-x-6 gap-y-2.5 max-sm:grid-cols-1">
+          <div className="flex flex-col gap-1 search-input">
+            <label
+              htmlFor="state"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              State
+            </label>
+            <input
+              id="state"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 search-input">
+            <label
+              htmlFor="city"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              City
+            </label>
+            <input
+              id="city"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 search-input">
+            <label
+              htmlFor="country"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              Country
+            </label>
+            <input
+              id="country"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 search-input">
+            <label
+              htmlFor="number"
+              className="text-[var(--color-main)] text-[12px] font-medium"
+            >
+              Phone Number
+            </label>
+            <input
+              id="number"
+              type="text"
+              className="px-3 text-[14px] font-semibold"
+            />
+          </div>
         </div>
 
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="country"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            Country
-          </label>
-          <input id="country" type="text" className="px-3 text-[14px]" />
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <label
-            htmlFor="number"
-            className="text-[var(--color-main)] text-[12px] font-medium"
-          >
-            Phone Number
-          </label>
-          <input id="number" type="text" className="px-3 text-[14px]" />
-        </div>
-
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 search-input">
           <label
             htmlFor="email"
             className="text-[var(--color-main)] text-[12px] font-medium"
           >
             Contact Email
           </label>
-          <input id="email" type="text" className="px-3 text-[14px]" />
+          <input
+            id="email"
+            type="text"
+            className="px-3 text-[14px] font-semibold"
+          />
         </div>
       </div>
 
-      <button className="py-[5px] px-[10px] text-[14px] bg-[var(--color-main)] text-white font-semibold rounded-[6px] cursor-pointer">
-        Search Patient
-      </button>
+      <div className="flex justify-center items-center gap-8">
+        <button className="py-[5px] px-[10px] text-[14px] font-semibold bg-[var(--color-main)] text-white  rounded-[6px] cursor-pointer">
+          Search Patient
+        </button>
+
+        <button
+          className="py-[5px] px-[10px] text-[14px] font-semibold bg-[#d9d9d9] text-white  rounded-[6px] cursor-pointer"
+          onClick={onHideSearchPatient}
+        >
+          Close
+        </button>
+      </div>
     </form>
   );
 };
 
-const Button = ({ children, secondary, ...props }) => {
+const Button = ({ children, secondary, onClick, ...props }) => {
   const btnClass = secondary
     ? "patient-btn patient-btn--secondary"
     : "patient-btn";
   return (
-    <button className={`text-[14px] ${btnClass}`} {...props}>
+    <button
+      className={`text-[14px] cursor-pointer ${btnClass}`}
+      {...props}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
